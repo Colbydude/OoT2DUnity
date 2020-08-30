1a682690819dd4503ea338bb638235a20a8bfc98
Brandon <rourke.brandon@hotmail.com>
2020-08-30 15:35:02 -0700

    Update ThrowSMB to set the Object's layer to ThrownObjects on enter and don't wait for the exit to let it collide

--- Assets/Scripts/Player/StateMachineBehaviours/ThrowSMB.cs
+++ Assets/Scripts/Player/StateMachineBehaviours/ThrowSMB.cs
@@ -9,14 +9,13 @@ public class ThrowSMB : SceneLinkedSMB<PlayerController>
         m_MonoBehaviour.SetMoveVector(new Vector2(0, 0));
 
         heldObject = m_MonoBehaviour.HeldObject;
+        heldObject.Collider2D.enabled = true;
+        heldObject.gameObject.layer = Constants.Layers.ThrownObjects;
         heldObject.Throw(m_MonoBehaviour.direction, m_MonoBehaviour.throwSpeed);
+        m_MonoBehaviour.HeldObject = null;
     }
 
     public override void OnSLStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // Enable the heldObject's collider after Link has finished the throw animation,
-        // so that Link himself doesn't get hit. Maybe a better way to do this?
-        heldObject.Collider2D.enabled = true;
-        m_MonoBehaviour.HeldObject = null;
     }
 }
